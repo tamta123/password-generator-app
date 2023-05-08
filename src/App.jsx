2df31e1f2397a033bad39ad29c9aa988565fe7ab
tamta123 <tamtag186@gmail.com>
2023-05-08 14:21:35 +0400

    375 px mark-up

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Password Generator</h1>
+      <main>
+        <div className="final-password">
+          <span className="password">something</span>
+          <svg width="21" height="24" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z"
+              fill="#A4FFAF"
+            />
+          </svg>
+        </div>
+        <div className="characteristics">
+          <div className="character-length">
+            <span className="length">Character Length</span>
+            <span className="number">10</span>
+          </div>
+          <div class="slide-container">
+            <input
+              className="slider"
+              type="range"
+              id="vol"
+              name="vol"
+              min="0"
+              max="20"
+              step="1"
+            />
+          </div>
+          <div className="checkbox">
+            <div>
+              <div className="box" id="Uppercase"></div>
+              <label for="Uppercase"> Include Uppercase Letters</label>
+            </div>
+            <div>
+              <div className="box" id="Lowercase"></div>
+              <label for="Lowercase"> Include Lowercase Letters</label>
+            </div>
+            <div>
+              <div className="box" id="Numbers"></div>
+              <label for="Numbers"> Include Numbers</label>
+            </div>
+            <div>
+              <div className="box" id="Symbols"></div>
+              <label for="Symbols"> Include Symbols</label>
+            </div>
+          </div>
+          <div className="strength">
+            <span class="title">STRENGTH</span>
+            <span className="strength-level">MEDIUM</span>
+            <div className="indicator">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+          <button className="generate">
+            GENERATE
+            <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill="#24232C"
+                d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"
+              />
+            </svg>
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
